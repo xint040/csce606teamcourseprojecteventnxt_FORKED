856dc8d998be1a0426ef93c4a1170d055f88cc0b
pkavu-1998-tamu <143028516+pkavu-1998-tamu@users.noreply.github.com>
2023-12-05 23:44:48 -0600

    Update development.rb

--- EventNXT_new_app/config/environments/development.rb
+++ EventNXT_new_app/config/environments/development.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   
-  config.hosts << "eventnxt-new-app-prod-73cbfd69694e.herokuapp.com"
+  config.hosts = ENV['ALLOWED_HOST']
   
   
   # <!--===================-->

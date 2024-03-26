ae637d9ec93d0e67ef860247bf34de8ba1eb2ff9
Alex Wise <alexwise@Alexs-MacBook-Pro.local>
2024-03-25 19:20:00 -0500

    fix no file upload

--- EventNXT_new_app/app/controllers/guests_controller.rb
+++ EventNXT_new_app/app/controllers/guests_controller.rb
@@ -119,6 +119,9 @@ class GuestsController < ApplicationController
   def import_guests_csv
     event_id = params[:event_id]
     event = Event.find(event_id)
+
+    return redirect_to event_path(@event), alert: "No file uploaded." if params[:file].blank?
+
   
     # Roo setup to open the Excel file
     file = params[:file]

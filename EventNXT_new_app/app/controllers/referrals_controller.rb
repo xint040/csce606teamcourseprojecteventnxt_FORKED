55526bf22a911102024392537296c4730d0c048a
xintong <xintong@tamu.edu>
2024-03-29 01:51:12 -0500

    modification on the controller of the referral feature

--- EventNXT_new_app/app/controllers/referrals_controller.rb
+++ EventNXT_new_app/app/controllers/referrals_controller.rb
@@ -1,6 +1,6 @@
 class ReferralsController < ApplicationController
-  before_action :set_guest
-  before_action :set_referral, only: %i[ edit update ]
+  #before_action :set_guest
+  #before_action :set_referral, only: %i[ edit update ]
   
     def new
 
@@ -12,7 +12,8 @@ class ReferralsController < ApplicationController
 
       UserMailer.referral_confirmation(friend_email).deliver_now
 
-      @referral = Referral.create(guest_id: @guest.id, email: @guest.email, name: '#{@guest.first_name} #{@guest.last_name}', referred: friend_email, ref_code: @guest.id)
+      @guest = Guest.find(id: ref_code)
+      @referral = Referral.create(guest_id: ref_code, email: @guest.email, name: '#{@guest.first_name} #{@guest.last_name}', referred: friend_email, ref_code: ref_code)
       @referral.save
 
       respond_to do |format|
@@ -30,49 +31,50 @@ class ReferralsController < ApplicationController
 
   # PATCH/PUT /referrals/1 or /referrals/1.json
     def update
+      @referral = Referral.find(params[:id])
       if @referral.reward_method == 'reward/ticket'
-        the_referral_parametrization = {
-           email: @referral.email, 
-           name: @referral.name, 
-           referred: @referral.referred, 
-           status: @referral.status, 
-           tickets: @referral.tickets, 
-           amount: @referral.amount, 
-           reward_method: 'reward/ticket', 
-           reward_input: params[:reward_input], 
-           reward_value: params[:reward_input] * tickets,
-           guest_id: @guest.id,
-           ref_code: @guest.id
-          }
-      
+        #the_referral_parametrization = {
+        #   email: @referral.email, 
+        #   name: @referral.name, 
+        #   referred: @referral.referred, 
+        #   status: @referral.status, 
+        #   tickets: @referral.tickets, 
+        #   amount: @referral.amount, 
+        #   reward_method: 'reward/ticket', 
+        #   reward_input: params[:reward_input], 
+        #   reward_value: params[:reward_input] * tickets,
+        #   guest_id: @guest.id,
+        #   ref_code: @guest.id
+        #  }
+        @referral.update(:reward_value => (params[:reward_input] * tickets))
       elsif @referral.reward_method == 'reward percentage %'
-        the_referral_parametrization = {
-           email: @referral.email, 
-           name: @referral.name, 
-           referred: @referral.referred, 
-           status: @referral.status, 
-           tickets: @referral.tickets, 
-           amount: @referral.amount, 
-           reward_method: 'reward percentage %', 
-           reward_input: params[:reward_input], 
-           reward_value: ((@referral.amount) * params[:reward_input]) / 100,
-           guest_id: @guest.id,
-           ref_code: @guest.id
-          }
+        #the_referral_parametrization = {
+        #   email: @referral.email, 
+        #   name: @referral.name, 
+        #   referred: @referral.referred, 
+        #   status: @referral.status, 
+        #   tickets: @referral.tickets, 
+        #   amount: @referral.amount, 
+        #   reward_method: 'reward percentage %', 
+        #   reward_input: params[:reward_input], 
+        #   reward_value: ((@referral.amount) * params[:reward_input]) / 100,
+        #   guest_id: @guest.id,
+        #   ref_code: @guest.id
+        #  }
       end
-      @referral.update(the_referral_parametrization)
+      @referral.update(:reward_value => ((@referral.amount) * params[:reward_input]) / 100)
     end
 
     private
      
-    def set_guest
-      @guest = Guest.find(params[:guest_id])
-    end
+    #def set_guest
+    #  @guest = Guest.find(params[:guest_id])
+    #end
 
     # Use callbacks to share common setup or constraints between actions.
-    def set_referral
-      @referral = @guest.referrals.find(params[:id])
-    end
+    #def set_referral
+    #  @referral = @guest.referrals.find(params[:id])
+    #end
 
     # Only allow a list of trusted parameters through.
     def referral_params
